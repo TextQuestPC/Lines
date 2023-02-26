2f1ee95f641a231ffcd5021a09f5fbae947d0c1d
VladNaboka <naboka.wlad@mail.ru>
2023-02-26 23:27:55 +0600

    error correction ditals with button

--- LinePaint/Assets/LanguageButton.cs
+++ LinePaint/Assets/LanguageButton.cs
@@ -2,13 +2,42 @@ using LinePaint;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using YG;
 
 public class LanguageButton : MonoBehaviour
 {
     [SerializeField] private UIManager_paint uIManager;
+
+    [SerializeField] private LanguageYG[] languageYG;
+
+    public string lang1;
+    public string lang2;
+    bool activ = false;
+    private void Update()
+    {
+        languageYG = FindObjectsOfType<LanguageYG>();
+        if (activ)
+        {
+            for (int i = 0; i < languageYG.Length; i++)
+            {
+                languageYG[i].SwitchLanguage(lang1);
+            }
+            uIManager.TotalDiamonds.text = "" + GameManager_paint.totalDiamonds;
+            uIManager.LevelText.text += (GameManager_paint.currentLevel + 1);
+        }
+        if (!activ)
+        {
+            for (int i = 0; i < languageYG.Length; i++)
+            {
+                languageYG[i].SwitchLanguage(lang2);
+            }
+            uIManager.TotalDiamonds.text = "" + GameManager_paint.totalDiamonds;
+            uIManager.LevelText.text += (GameManager_paint.currentLevel + 1);
+        }
+    }
     public void buttonLanguage()
     {
-        uIManager.TotalDiamonds.text = "" + GameManager_paint.totalDiamonds;
-        uIManager.LevelText.text += (GameManager_paint.currentLevel + 1);
+        activ = !activ;
+
     }
 }
